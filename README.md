b79ba2084cbd1a9da97812b567bab27811891d08
sunxiran <maricle01@163.com>
2019-04-08 10:07:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## The Knowledge Web Project
---
+++ src/main/java/com/sxr/knowledge/KnowledgeWebApplication.java
@@ -0,0 +1,13 @@
+package com.sxr.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeWebApplication.class, args);
+	}
+
+}
