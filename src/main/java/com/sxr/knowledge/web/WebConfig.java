eccbf3cff541893701424754c2f496aaeeb13c0f
sunxiran <maricle01@163.com>
2019-04-23 20:16:15 +0800

    初步添加react

--- src/main/java/com/sxr/knowledge/web/WebConfig.java
+++ src/main/java/com/sxr/knowledge/web/WebConfig.java
@@ -3,6 +3,7 @@ package com.sxr.knowledge.web;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -11,6 +12,12 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 
 public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/");
+	}
 
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
